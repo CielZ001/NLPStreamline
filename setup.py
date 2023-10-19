c4f6bcfe8aaec6586f9a5b6f0a19c6b247e60578
Ciel (Xi) <122921783+CielZ001@users.noreply.github.com>
2023-10-19 16:08:59 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="nlpstreamline",
+    version="0.0.10",
+    description="Streamline basic text processing & NLP",
+    package_dir={"": "nlpstreamline"},
+    packages=find_packages(where="nlpstreamline"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArjanCodes/2023-package",
+    author="ArjanCodes",
+    author_email="arjan@arjancodes.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
